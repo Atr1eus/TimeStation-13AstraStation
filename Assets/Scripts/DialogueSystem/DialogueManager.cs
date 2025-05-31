2e5a1593093cc7a72e42485065662ac648ce5b5b
Atr1eus <115001696+Atr1eus@users.noreply.github.com>
2025-06-01 03:32:08 +0800

    dialogue-v2

---
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueSystem;
+
+public class DialogueManager : MonoBehaviour
+{
+    Dialogue dialogueManager;
+
+    [Header("对话设置")]
+    public DialogueGraph graph;
+    public DialogueTheme alternativeTheme;
+
+    //[Header("Game State Variables")]
+    //public List<DialogueGameState> gameStateVariables = new List<DialogueGameState>();
+
+    [Header("输入控制")]
+    public KeyCode advanceKey = KeyCode.Space;
+    public KeyCode speedUpKey = KeyCode.T;
+    public KeyCode themeTestKey = KeyCode.S;
+    public KeyCode pauseKey = KeyCode.P;
+
+    [Header("UIHandler")]
+    public DialogueUIHandler UiHandler;
+
+    [Header("事件")]
+    public DialogueEvent EventList;
+
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+
+    private void Start()
+    {
+        dialogueManager = Dialogue.instance;
+
+        dialogueManager.dialogueCallbackActions.OnNodeLeave += OnNodeLeave;
+        dialogueManager.dialogueCallbackActions.OnNodeEnter += OnNodeEnter;
+
+        dialogueManager.OnChoiceDraw += OnChoiceDraw;
+
+        dialogueManager.RegisterEventHandler(EventHandler);
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    //输入处理
+    private void HandleInput()
+    {
+        // 对话推进控制
+        if (Input.GetKeyDown(advanceKey))
+        {
+            if (dialogueManager.IsRunning)
+            {
+                if (dialogueManager.isAnimating)
+                {
+                    dialogueManager.EndLine(); // 跳过文本动画
+                }
+                else if (dialogueManager.CurrentState != DialogueState.AwaitingEventResponse)
+                {
+                    dialogueManager.AdvanceDialogue(); // 推进对话
+                }
+            }
+            else
+            {
+                dialogueManager.StartDialogue(); // 开始对话
+            }
+        }
+
+        // 文本加速显示
+        if (Input.GetKeyDown(speedUpKey))
+        {
+            dialogueManager.ToggleSpeedUp(!dialogueManager.speedUp);
+        }
+
+        // 主题测试
+        if (Input.GetKeyDown(themeTestKey) && alternativeTheme != null)
+        {
+            //alternativeTheme.ChangeColor("red", Color.green);
+        }
+
+        // 暂停/恢复对话
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (dialogueManager.CurrentState == DialogueState.Paused)
+            {
+                dialogueManager.Pause(false);
+            }
+            else
+            {
+                dialogueManager.Pause(true);
+            }
+        }
+    }
+
+    // 文本节点开始时的回调
+    public void OnLineStartCallback(TextNode node)
+    {
+        // TODO
+
+    }
+
+    // 文本节点结束时的回调
+    public void OnLineEndCallback(TextNode node)
+    {
+        UiHandler.DisableNamePlate();
+    }
+
+    // 结点进入时的回调
+    public void OnNodeEnter(BaseNode node)
+    {
+        //Debug.Log("Entered " + node.name);
+    }
+
+    // 结点离开时的回调
+    public void OnNodeLeave(BaseNode node)
+    {
+        //Debug.Log("left " + node.name);
+    }
+
+    //绘制选项
+    public bool OnChoiceDraw(DialogueChoices node)
+    {
+        UiHandler.RenderDialogueChoices(node, dialogueManager);
+        return true;
+    }
+
+    public void EventHandler(DialogueEvent myEvent)
+    {
+        if (myEvent != null)
+        {
+            Debug.Log(myEvent.eventName);
+            if (myEvent.eventName == "End")
+            {
+                dialogueManager.Ui.ShowDialoguePane(false);
+                if(Input.GetKeyDown(KeyCode.Space))
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+
+            if (myEvent.eventName == "yourEvent")
+            {
+
+            }
+        }
+    }
+    
+}
---
+++ Assets/Scripts/DialogueSystem/DialogueUIHandler.cs
@@ -0,0 +1,69 @@
+using DialogueSystem;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueUIHandler : MonoBehaviour
+{
+    public Transform ChoicePanel;
+    public GameObject DialogueWindow;
+    public Button choiceButtonPrefab;
+    public Transform NamePanel;
+
+    private List<Button> activeChoiceButtons = new List<Button>();
+
+    void Start()
+    {
+        ChoicePanel.gameObject.SetActive(false);
+
+    }
+
+    public void RenderDialogueChoices(DialogueChoices dialogueChoices, Dialogue dialogueSystem)
+    {
+        //DialogueWindow.SetActive(false);
+        ChoicePanel.gameObject.SetActive(true);
+        foreach (DialogueChoice choice in dialogueChoices.Choices)
+        {
+            Button choiceButton = Instantiate(choiceButtonPrefab, ChoicePanel);
+            TMP_Text btnText = choiceButton.transform.GetChild(0).GetComponent<TMP_Text>();
+            btnText.text = choice.text;
+            choiceButton.onClick.AddListener(() => {
+                ClearAllOptions();
+                dialogueSystem.SelectDialogueChoice(choice.choiceNumber);
+                });
+            activeChoiceButtons.Add(choiceButton);
+        }
+    }
+
+    public void ClearAllOptions()
+    {
+        // 隐藏选择面板
+        ChoicePanel.gameObject.SetActive(false);
+
+        // 销毁所有按钮实例
+        foreach (Button button in activeChoiceButtons)
+        {
+            if (button != null)
+            {
+                Destroy(button.gameObject);
+            }
+        }
+
+        // 清空列表
+        activeChoiceButtons.Clear();
+    }
+
+    public void SetSpeakerName(string name)
+    {
+        NamePanel.gameObject.SetActive(true);
+        TMP_Text text = NamePanel.GetChild(0).GetComponent<TMP_Text>();
+        text.text = name;
+    }
+
+    public void DisableNamePlate()
+    {
+        NamePanel.gameObject.SetActive(false);
+    }
+}
