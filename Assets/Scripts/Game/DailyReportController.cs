62e4ab57d0c5d8547b10c69148b5085e3b2fc55f
Alkalier <2964671387@qq.com>
2025-06-04 15:38:51 +0800

    1.0.2final

---
+++ Assets/Scripts/Game/DailyReportController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DailyReportController : MonoBehaviour
+{
+    [Header("UI≈‰÷√")]
+    public Image reportImage;
+    public Text decreaseItemHeaderText;
+    public Text totalItemHeaderText;
+
+
+}
