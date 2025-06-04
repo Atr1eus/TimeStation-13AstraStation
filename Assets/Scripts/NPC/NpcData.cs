c24d9c95a004de735cdbf94a477f1ffaf452e3ef
Atr1eus <115001696+Atr1eus@users.noreply.github.com>
2025-06-04 15:53:06 +0800

    TicketDataWarning

--- Assets/Scripts/NPC/NpcData.cs
+++ Assets/Scripts/NPC/NpcData.cs
@@ -1,8 +1,6 @@
 using DialogueSystem;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 /// <summary>
 /// npc数据配置类 只关心npc是什么
 /// </summary>
@@ -16,60 +14,48 @@ public enum NpcSex
     Man,
     Woman
 }
-public enum NpcCareer
-{
-    Teacher,
-    Doctor,
-    None
-}
-public enum NpcSpecies
-{
-    Human,
-    Cat
-}
 [CreateAssetMenu(fileName = "New NPC", menuName = "Game/NPC")]
 public class NpcData : ScriptableObject
 {
+
+
+    [Header("npc通用配置")]
     public int age;
     public string npcName;
     public NpcSex sex;
-    public GameObject hand; //��(����npc��)
-    public NpcType type; //��ͨ/����
-    public NpcCareer career; //ְҵ
-    public NpcSpecies species; //����
-    public TravelTo travelTo; //ȥ��ȥ����δ��
-    [TextArea] public string npcDescription; //��ò�Ƚ���
-
-
-    [Header("对话配置")]
-    public DialogueGraph npcDialogue; //初始对话列表
-
-    [Header("交易配置")]
+    public NpcType type;
+    public TravelTo travelTo;
+    public DialogueGraph npcDialogue;
     public List<ItemData> requestItems; //索取物品列表
     public List<ItemData> offerItems; //带来物品列表
-
-    [Header("出现时间配置")]
-    public int minAppearRound; //最早出现回合
-    public int maxAppearRound; //最晚出现回合
-    [Header("Npc金钱奖励")]
     public int minPerRequestAwardGolds; //每交易一个需求物品可能获得的最低金钱
     public int maxPerRequestAwardGolds; //每交易一个需求物品可能获得的最高金钱
     public int minPerNonrequestAwardGolds; //每交易一个非需求物品可能获得的最低金钱
     public int maxPerNonrequestAwardGolds; //每交易一个非需求物品可能获得的最高金钱
-    public int trueChoiceAwardGolds; //ѡ����ȷ������Ǯ
-    public int falseChoicePunishGolds; //ѡ�����ͷ���Ǯ
-    [Header("Npc好感度配置")]
+    public int trueChoiceAwardGolds;
+    public int falseChoicePunishGolds;
     public int initialFavorability;
 
-    [Header("Npcȥ����ȥ/δ����������")]
-    public List<string> reasonToFurture; //ȥ��δ�������б�
-    public List<string> reasonToPast; //ȥ����ȥ�����б�
+    [Header("仅剧情npc配置")]
+    public GameObject hand;
+    public List<TicketData> ticket;
+    public List<string> storyNpcDescriptions;
+    public List<int> storyNpcMinAppearRounds;
+    public List<int> storyNpcMaxAppearRounds;
+    public List<string> storyNpcReasons;
 
-    [Header("NPC出现条件")]
+    [Header("仅路边npc配置")]
+    public string npcDescription;
+    public int minAppearRound; //最早出现回合
+    public int maxAppearRound; //最晚出现回合
+    public List<string> reasonToFurture;
+    public List<string> reasonToPast;
+
+    [Header("该npc出现条件")]
     public bool isMoneyMore10K;
     public bool isRoundMore5;
 
-    //public string GetRandomGreeting() => greetingDialogues[Random.Range(0, greetingDialogues.Count)]; //获取随机招呼语
+
 
     public ItemData GetRandomRequestItem() => requestItems[Random.Range(0, requestItems.Count)]; //获取随机物品要求
     public ItemData GetRandomOfferItem() => offerItems[Random.Range(0, offerItems.Count)];
