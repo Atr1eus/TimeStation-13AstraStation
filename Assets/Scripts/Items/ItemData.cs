c4bafeb24b472c257da6fc21c238836901751d12
Alkalier <2964671387@qq.com>
2025-06-04 15:49:16 +0800

    data冲突解决

--- Assets/Scripts/Items/ItemData.cs
+++ Assets/Scripts/Items/ItemData.cs
@@ -10,13 +10,12 @@ public enum ItemType
 /// 物品数据类
 /// </summary>
 [CreateAssetMenu(fileName = "New Item", menuName = "Items/Item Data")]
-[System.Serializable]
 public class ItemData : ScriptableObject
 {
     public string itemName;
     public string itemID; //物品唯一标识
     public int itemPrice;
-    public GameObject prefab;
+    public Sprite icon;
     [TextArea] public string itemDescription; //物品描述
 
     [Header("索要/交易配置")]
