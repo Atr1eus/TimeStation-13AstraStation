4179eb34693934c5ae8fcb8eb813d655621bd7a0
Atr1eus <nero20121212@outlook.com>
2025-06-06 14:59:37 +0800

    更改对话推进方式

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -89,7 +89,7 @@ public class DialogueManager : MonoBehaviour
     private void HandleInput()
     {
         // 对话推进控制
-        if (Input.GetKeyDown(advanceKey)||Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(advanceKey))
         {
 
             if (!dialogueManager.IsRunning && isTalking)
